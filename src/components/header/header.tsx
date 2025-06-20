5c0fb22cf667cda44830d57689122d94eecbe6bd
Aravindhan R <aravindhan.16@outlook.com>
2025-06-20 14:40:57 +0530

    header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+interface HeaderProps {
+    organizationName?: string;
+    organizationLogo?: string;
+    onLogoClick?: () => void;
+    onNavLinkClick?: (link: string) => void;
+    navLinks?: { name: string; iconClass: string; id: string }[];
+}
+const Header: React.FC<HeaderProps> = ({ organizationName, organizationLogo, onLogoClick, onNavLinkClick, navLinks }) => {
+  return (
+    <header className="header"> 
+      <div className="logo">
+      </div>
+      <nav className="nav-links">
+      
+      </nav>
+    </header>
+  );
+}
+export default Header;
